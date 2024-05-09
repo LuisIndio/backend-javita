9e9844dd84028d8356d4e1ddf225b371535c69e0
torricosergio0@gmail.com <SergioTorricoArauz>
2024-05-09 16:56:32 -0400

    Primer commit crud de user completo

--- src/test/java/com/register/Register2ApplicationTests.java
+++ src/test/java/com/register/Register2ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.register;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Register2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
